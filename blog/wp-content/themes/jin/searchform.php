84fab85cfde4789ae364e6eb829f98fd621b1370
Taichi <gciuzi52@gmail.com>
2021-02-08 16:16:51 +0100

    add gulp & remove blog & move to slim

--- blog/wp-content/themes/jin/searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="search-box" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<input type="search" placeholder="" class="text search-text" value="" name="s" id="s">
-	<input type="submit" id="searchsubmit" value="&#xe931;">
-</form>
